4b97818c5fe25f75b701ad7f90ae3fc3e798081d
wei.hu <wei.hu@snowballtech.com>
2026-07-28 14:08:07 +0800

    Fix TI cc27xx C3 buffer member layout

--- src/platform/ti/cc27xx/BLEManagerImpl.h
+++ src/platform/ti/cc27xx/BLEManagerImpl.h
@@ -293,6 +293,9 @@ private:
 
     uint8_t mAdvDatachipOBle[CHIPOBLE_ADV_DATA_MAX_SIZE];
     uint8_t mScanResDatachipOBle[CHIPOBLE_ADV_DATA_MAX_SIZE];
+#if CHIPOBLE_ENABLE_C3
+    System::PacketBufferHandle mC3AdditionalDataBufferHandle;
+#endif
 
     ClockP_Struct clkRpaRead;
     ClockP_Struct clkAdvTimeout;
@@ -332,7 +335,6 @@ private:
     static void FastAdvTimeoutHandler(uintptr_t arg);
     static void CHIPoBLEProfile_charValueChangeCB(uint8_t paramId, uint16_t len, uint16_t connHandle);
 #if CHIPOBLE_ENABLE_C3
-    System::PacketBufferHandle mC3AdditionalDataBufferHandle;
     static bStatus_t CHIPoBLEProfile_readAdditionalDataCB(uint8_t * value, uint16_t * len, uint16_t maxLen);
 #endif
     static void PasscodeCb(uint8_t * pDeviceAddr, uint16_t connHandle, uint8_t uiInputs, uint8_t uiOutputs, uint32_t numComparison);
