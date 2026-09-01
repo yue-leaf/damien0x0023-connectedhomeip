c4fc58f795d31962c74c103a6ebfc350f2c77895
wei.hu <wei.hu@snowballtech.com>
2026-09-01 19:27:56 +0800

    update examples-efr32-factory.yaml

--- examples/lighting-app/silabs/include/AppConfig.h
+++ examples/lighting-app/silabs/include/AppConfig.h
@@ -25,7 +25,7 @@
 
 #define APP_TASK_NAME "Lit"
 
-#define BLE_DEV_NAME "TI-Light"
+#define BLE_DEV_NAME "SiLabs-Light"
 
 // Time it takes in ms for the simulated actuator to move from one
 // state to another.
